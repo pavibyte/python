7f17968e7c1207a37403faabe6d8c38aa485b3f8
pavibyte <learnpavi123@gmail.com>
2024-08-11 18:46:44 +0530

    Update string.py

--- chapter 4/string.py
+++ chapter 4/string.py
@@ -1,1 +1,17 @@
+#string data type
+first="pavithra" #literal assignment that is literally assigning a value
+last="darsini"   #string literal
+print('')        #first line output is empty line
+
+print(type(first))             #<class 'str'>
+print(type(last)== str)        #True
+print(type(last)== "str")      #false because the data type is under quotation
+print(isinstance(first,str))   #True
+
+#constuctor function
+pizza="pepperoni"
+print(type(pizza))
+print(type(pizza)== str)
+print(isinstance(pizza,str))
+
 
