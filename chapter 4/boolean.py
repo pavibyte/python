72b26abdc7aeb19702b7f965ec4e8ea0280af220
pavibyte <learnpavi123@gmail.com>
2024-08-12 18:51:27 +0530

    Update boolean.py

--- chapter 4/boolean.py
+++ chapter 4/boolean.py
@@ -1,1 +1,4 @@
-
+myvalue=False                      #must start with capital T or F
+x=bool(False)
+print(type(x))                     # <class 'bool'>       
+print(isinstance(myvalue,bool))    # True
